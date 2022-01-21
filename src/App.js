efbab0c64470e6d233ba432b0093a01409064f9e
hardlywave <45406812+hardlywave@users.noreply.github.com>
2022-01-21 13:05:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Route, useParams, Routes } from "react-router-dom";
-//
+
 import Header from "./Header";
 import Definition from "./Definition";
 import ErrorPage from "./ErrorPage";
-//
+
 const NewComponent = ({ word, meanings, setMeanings, setLoading }) => {
   let { word: url_word } = useParams();
   useEffect(() => {
@@ -25,7 +25,7 @@ const NewComponent = ({ word, meanings, setMeanings, setLoading }) => {
   }, [url_word]);
   return <Definition word={word || url_word} meanings={meanings} />;
 };
-//
+
 function App() {
   const [word, setWord] = useState("");
   const [meanings, setMeanings] = useState([]);
